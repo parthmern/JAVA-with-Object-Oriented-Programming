2dfa88f5ac9e311b5e4e89b765ed40c9b45050dd
parthmern <125397720+parthmern@users.noreply.github.com>
2023-06-27 23:06:02 +0530

    Add files via upload

---
+++ 01_Introduction/01_Output.java
@@ -0,0 +1,44 @@
+// static or Dynamic
+// Java is Static because we have to declare DATATYPES (int,char)
+// dynamic means no declaration by using DATATYPES ex.javascript(let x , var y)
+
+public class main {
+    public static void main(String[] args) {
+        
+        System.out.print("hello world");
+        
+    }
+}
+
+// first class k baad { <-- open body curlybracket
+// } <-- closebody
+
+// KEYWORDS = predefined words
+// real life ex. instagram handel username
+// wo already kisi ka ho gya hai - so you cannot use this
+//-------------------------------------------------------------------
+
+// Min the first line MAIN is the name of file
+// PUBLIC -- publically accessible from anywhere (from other files,other classes)
+// we can change public to PRIVATE -- not excecution
+// STATIC -- without declaring the object we can run the program
+// VOID -- no return value
+// MAIN -- main function from here the program start -- entry point of excecution
+// STRING[] -- parameters -- commandline arguments
+// ARGS --  arguments 
+
+//-------------------------------------------------------------
+// main(string[] args)  ==> here (parameter)
+// System.out.println("ARGUMENTS")
+
+
+
+
+
+
+
+
+
+
+
+
